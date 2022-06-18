634baa53305c5cb037ad88699e18454b7cceaabd
Sankesh Jain <ramojitinorange@gmail.com>
2022-06-18 09:06:19 +0530

    Fixed Error

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,31 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-<div>
-      <div className="section container text-center" id="1">
-        <div className="row">
-          <div className="col-sm-6 p-5">
-            <img src={yourWeatherLogo} alt="Rain" className="img-fluid"/>
-          </div>
-          <div className="col-sm-6 d-flex flex-column justify-content-center">
-            <WeatherInput btnclick={(ele) => onLoading(ele)}></WeatherInput>
-          </div>
+  <div>
+    <div className="section container text-center" id="1">
+      <div className="row">
+        <div className="col-sm-6 p-5">
+          <img src="{yourWeatherLogo}" alt="Rain" className="img-fluid" />
+        </div>
+        <div className="col-sm-6 d-flex flex-column justify-content-center">
+          <!----<WeatherInput btnclick={(ele) => onLoading(ele)}></WeatherInput>-->
         </div>
       </div>
-      <div className="section container text-center" id="2">
-        <div className="row">
-          <div className="col-sm-6 d-flex justify-content-center align-items-center p-5">
-            {weather_icon_out}
-          </div>
-          <div className="col-sm-6">
-            <div className="d-flex justify-content-center p-5">
-              {weather_out}
-            </div>
-          </div>
+    </div>
+    <div className="section container text-center" id="2">
+      <div className="row">
+        <div
+          className="col-sm-6 d-flex justify-content-center align-items-center p-5"
+        >
+          {weather_icon_out}
+        </div>
+        <div className="col-sm-6">
+          <div className="d-flex justify-content-center p-5">{weather_out}</div>
         </div>
       </div>
-    </div></template>
+    </div>
+  </div>
+</template>
 
 <style>
 .section {
