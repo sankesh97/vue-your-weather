c1b74f82f125029b8f37c2c9a028ff9ee469a94b
Sankesh Jain <ramojitinorange@gmail.com>
2022-06-18 09:02:23 +0530

    second-commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,50 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+<div>
+      <div className="section container text-center" id="1">
+        <div className="row">
+          <div className="col-sm-6 p-5">
+            <img src={yourWeatherLogo} alt="Rain" className="img-fluid"/>
+          </div>
+          <div className="col-sm-6 d-flex flex-column justify-content-center">
+            <WeatherInput btnclick={(ele) => onLoading(ele)}></WeatherInput>
+          </div>
+        </div>
+      </div>
+      <div className="section container text-center" id="2">
+        <div className="row">
+          <div className="col-sm-6 d-flex justify-content-center align-items-center p-5">
+            {weather_icon_out}
+          </div>
+          <div className="col-sm-6">
+            <div className="d-flex justify-content-center p-5">
+              {weather_out}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div></template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.section {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.main-btn {
+  border: none;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  background-color: teal;
+}
+
+.main-btn:hover {
+  background-color: #505050;
+  color: #fff;
 }
 </style>
